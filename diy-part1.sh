66b8dbc78cd0302ca38ce0b61ba87f043445051a
RamblerSY <younger911@gmail.com>
2025-01-06 23:11:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,8 +6,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # homeproxy
-git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+#git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
